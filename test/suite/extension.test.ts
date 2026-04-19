a98da79eb8b77ae9ab9d018afe831644a9a454e8
Vitaly Adadurov <adva-cg@yandex.ru>
2026-04-19 07:28:31 +0500

    Добавлены тесты

--- test/suite/extension.test.ts
+++ test/suite/extension.test.ts
@@ -10,6 +10,31 @@ suite('Extension Test Suite', () => {
 	});
 
     test('Extension should be present', () => {
-        assert.ok(vscode.extensions.getExtension('user.changegittogoogledrive-extension'));
+        assert.ok(vscode.extensions.getExtension('VitalyAdadurov.changegittogoogledrive-extension'));
+    });
+
+    test('All commands should be registered', async () => {
+        const extension = vscode.extensions.getExtension('VitalyAdadurov.changegittogoogledrive-extension');
+        if (extension) {
+            await extension.activate();
+        }
+
+        const allCommands = await vscode.commands.getCommands(true);
+        const expectedCommands = [
+            'changegittogoogledrive-extension.setupGoogleCredentials',
+            'changegittogoogledrive-extension.authenticateWithGoogle',
+            'changegittogoogledrive-extension.initialUpload',
+            'changegittogoogledrive-extension.cloneFromGoogleDrive',
+            'changegittogoogledrive-extension.sync',
+            'changegittogoogledrive-extension.installGitHooks',
+            'changegittogoogledrive-extension.manageSyncHash',
+            'changegittogoogledrive-extension.configureAIHistorySync',
+            'changegittogoogledrive-extension.syncAIHistory',
+            'changegittogoogledrive-extension.toggleClipboardSync'
+        ];
+
+        for (const cmd of expectedCommands) {
+            assert.ok(allCommands.includes(cmd), `Command ${cmd} is not registered`);
+        }
     });
 });
